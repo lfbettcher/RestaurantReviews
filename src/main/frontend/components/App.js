4905c9d2c663b7e9fbf13fd9d482e02fec221dcb
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2021-05-24 19:38:42 -0700

    Initial Commit

---
+++ src/main/frontend/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import "regenerator-runtime/runtime";
+
+const app = () => {
+  return (
+    <div>
+      <h1>App.js</h1>
+    </div>
+  );
+};
+
+export default app;
