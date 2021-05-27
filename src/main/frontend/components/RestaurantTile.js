f5d8c37af62e32a92604f185b7be55ba5968ce9e
Chenxi Wang <56898616+wang-chenxi@users.noreply.github.com>
2021-05-27 14:00:07 -0700

    new styling

--- src/main/frontend/components/RestaurantTile.js
+++ src/main/frontend/components/RestaurantTile.js
@@ -23,20 +23,8 @@ const RestaurantTile = (props) => {
       <div className="restaurants__col">
         <img className="img-link" src={imgUrl}></img>
         <div className="restaurants-container">
-<<<<<<< HEAD
           <span className="restaurants__name"><Link to={`/restaurants/${id}`} className="restaurants__name-link">{name}</Link></span>
           <a href={websiteUrl}>Visit restaurant website</a>
-=======
-          <span className="restaurants__name">
-            <Link
-              to={`/restaurants/show/${id}`}
-              className="restaurants__name-link"
-            >
-              {name}
-            </Link>
-          </span>
-          <a href={websiteUrl}>{websiteUrl}</a>
->>>>>>> a5a938b83aad2b899c0d2e9c3250fce67c35f4c9
           {/* <p>{websiteUrl}</p> */}
           <p>{phoneNumber}</p>
           <p>{address}</p>
