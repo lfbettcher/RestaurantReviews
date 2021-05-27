39d46803c6945bdfc6ca165eafa2d17780082b1b
Rocky Cifu <Rocky.Cifu@yahoo.com>
2021-05-27 08:03:55 -0700

    Finished React Components Related to ShowPage

--- src/main/frontend/components/ReviewForm.js
+++ src/main/frontend/components/ReviewForm.js
@@ -1,10 +1,77 @@
-import React from 'react'
+import _ from "lodash"
+import React, { useState } from "react"
+import DBUtils from "../utils/DBUtils.js"
+import TextInput from "./TextInput.js"
+import SuccessTile from "./SuccessTile.js"
 
-const ReviewForm = () => {
-  return (
-    <div>
-      
-    </div>
+const ReviewForm = (props) => {
+  const INITIAL_STATE = {
+    reviewerName: "",
+    starRating: "",
+    review: "",
+    restaurant: props.restaurant,
+  }
+
+  const [values, setValues] = useState(INITIAL_STATE)
+  const [errors, setErrors] = useState({})
+  const [success, setSuccess] = useState(false)
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setValues({ ...values, [e.currentTarget.name]: e.currentTarget.value })
+  }
+
+  const handleSuccess = () => {
+    setSuccess(true)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const fields = ["reviewerName", "starRating", "review"]
+
+    if (DBUtils.isValid(fields, values, setErrors)) {
+      const response = await DBUtils.postData("/api/v1/reviews", values)
+
+      if (!response.id) {
+        setValues(INITIAL_STATE)
+        handleSuccess()
+      } else {
+        setErrors(response)
+      }
+    }
+  }
+
+  return success ? (
+    <SuccessTile />
+  ) : (
+    <form onSubmit={handleSubmit}>
+      <TextInput
+        label='reviewerName'
+        text='Name: '
+        onChange={handleChange}
+        value={values.reviewerName}
+        error={errors.reviewerName}
+      />
+      <br />
+      <TextInput
+        label='starRating'
+        text='Rating: '
+        onChange={handleChange}
+        value={values.starRating}
+        error={errors.starRating}
+      />
+      <br />
+      <TextInput
+        label='review'
+        text='Review: '
+        onChange={handleChange}
+        value={values.review}
+        error={errors.review}
+      />
+      <br />
+      <input className='submit-btn' type='submit' value='Submit' />
+    </form>
   )
 }
 
